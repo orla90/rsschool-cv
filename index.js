8de109784fdbdc805a61d9bfe300015499782f14
orla90 <orla90@inbox.ru>
2022-12-30 12:56:11 +0300

    feat: add adaptive menu to gh-pages

--- index.js
+++ index.js
@@ -1,7 +1,77 @@
+import PROJECTS from './projects.js';
 import i18Obj from './translate.js';
 
-const ruSwitcher = document.getElementsByClassName('switcher-item__ru')[0];
-const enSwitcher = document.getElementsByClassName('switcher-item__en')[0];
+window.onload = function () {
+  addProjects();
+  getTranslate(lang);
+};
+
+const addProjects = () => {
+  createContentFromList();
+  createContentFromImages();
+};
+
+const createContentFromList = () => {
+  const projectsList = document.getElementsByClassName('projects__list')[0];
+  PROJECTS.forEach((project) => {
+    const listItem = document.createElement('li');
+    listItem.classList.add('projects__item');
+    const link = createLinkWithText(project);
+    const imgWrapper = createImg(project, 'projects__img-wrapper');
+    listItem.append(link);
+    listItem.append(imgWrapper);
+    projectsList.append(listItem);
+  });
+};
+
+const createContentFromImages = () => {
+  const projectsList = document.getElementsByClassName('projects__images')[0];
+  PROJECTS.forEach((project) => {
+    const wrapper = document.createElement('div');
+    const link = createLink(project, 'link_images');
+    const imgWrapper = createImg(project, 'projects__images-wrapper');
+    link.append(imgWrapper);
+    wrapper.append(link);
+    projectsList.append(wrapper);
+  });
+};
+
+const createImg = (project, className) => {
+  const imgWrapper = document.createElement('div');
+  imgWrapper.classList.add(className);
+  const img = document.createElement('img');
+  img.classList.add('projects__img');
+  img.src = `${project.img}`;
+  imgWrapper.append(img);
+  return imgWrapper;
+};
+
+const createLinkWithText = (project) => {
+  const link = createLink(project, 'link_brown');
+  const icon = document.createElement('span');
+  icon.classList.add('ico', 'ico_project');
+  const description = document.createElement('span');
+  description.classList.add('link_text');
+  description.setAttribute('data-i18', `${project.name}`);
+  link.append(icon);
+  link.append(description);
+  return link;
+};
+
+const createLink = (project, className) => {
+  const link = document.createElement('a');
+  link.classList.add('link', className);
+  link.setAttribute('href', `${project.href}`);
+  link.setAttribute('target', '_blank');
+  return link;
+};
+
+const ruSwitcher = document.getElementsByClassName(
+  'language-switcher__item_ru'
+)[0];
+const enSwitcher = document.getElementsByClassName(
+  'language-switcher__item_en'
+)[0];
 let activeSwitcher = enSwitcher;
 let lang = activeSwitcher.innerText.toLowerCase();
 
@@ -25,3 +95,42 @@ const getTranslate = (lang) => {
     (textItem) => (textItem.textContent = i18Obj[lang][textItem.dataset.i18])
   );
 };
+
+/* Handle hamburger menu */
+const hamburger = document.getElementsByClassName('hamburger-icon')[0];
+const overlay = document.getElementsByClassName('overlay')[0];
+const nav = document.getElementsByClassName('nav__list')[0];
+
+const checkOverlay = () => {
+  if (overlay.classList.contains('open')) {
+    if (!document.body.classList.contains('fixed')) {
+      document.body.classList.add('fixed');
+    }
+  } else {
+    document.body.classList.remove('fixed');
+  }
+};
+
+const toggleOpenClass = () => {
+  hamburger.classList.toggle('open');
+  nav.classList.toggle('open');
+  overlay.classList.toggle('open');
+  checkOverlay();
+};
+
+overlay.addEventListener('click', () => {
+  toggleOpenClass();
+});
+
+hamburger.addEventListener('click', () => {
+  toggleOpenClass();
+});
+
+nav.addEventListener('click', (e) => {
+  if (
+    e.target.classList.contains('nav__link') &&
+    hamburger.classList.contains('open')
+  ) {
+    toggleOpenClass();
+  }
+});
